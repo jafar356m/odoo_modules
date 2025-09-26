e99692af8ac7574fc1124084fb69f8eb661e0908
Muhsina <c.muhsina@abrusnetworks.com>
2025-09-26 12:16:40 +0530

    odoo13 pos works for receipts

---
+++ pos_print_choice/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'POS Print Choice (Consolidate / Individual)',
+    'summary': 'Popup on Validate to choose consolidated or individual printing of receipts in POS',
+    'version': '13.0.1.0.0',
+    'category': 'Point of Sale',
+    'author': 'Your Company',
+    'website': 'https://example.com',
+    'license': 'LGPL-3',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'qweb': [
+        'static/src/xml/print_choice.xml',
+        'static/src/xml/print_choice_popup.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
